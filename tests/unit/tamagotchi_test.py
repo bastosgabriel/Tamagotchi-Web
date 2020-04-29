4a0c5c54fc631ed0465f9c3f2aa05e7e37681f9e
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-04-29 09:20:39 -0300

    Create base test structure

---
+++ tests/unit/tamagotchi_test.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from resources.tamagotchi import tamagotchi
+
+class tamagotchiTest(TestCase):
+    def test_cuddle(self):
+        pass
+
+    pass
+
+
+class itemTest(TestCase):
+    def test_snack(self):
+        pass
+    
+
+    pass
